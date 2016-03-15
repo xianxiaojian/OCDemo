bb64ff55d624d8acf025250389ea36d2d7c82ece
LinJing <linjing@linjingdemac-mini.local>
2016-03-15 15:46:39 +0800

    Initial Commit

---
+++ OCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCDemo
+//
+//  Created by linjing on 16/3/15.
+//  Copyright © 2016年 linjing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCDemo
+//
+//  Created by linjing on 16/3/15.
+//  Copyright © 2016年 linjing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
