1e140fa7a9cb4f9f4ec4c1f886edbee8aee5212a
LinJing <linjing@appledemacbook-pro-5.local>
2016-11-13 10:49:22 +0800

    change vc 00

--- OCDemo/Other/ViewController.m
+++ OCDemo/Other/ViewController.m
@@ -19,7 +19,7 @@ static NSString *const kCellId = @"cell";
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    //abcdefg
+    
     UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
     tableView.delegate = self;
     tableView.dataSource = self;
